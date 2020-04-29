e0bd1a43b1e1ba1cf034dae5b7da80cb901274c9
Thav0 <gustavodnz035@gmail.com>
2020-04-29 13:43:14 -0300

    input form @uniform/web

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { FiMail, FiLock, FiUser, FiChevronLeft } from 'react-icons/fi';
+
+import { Form } from '@unform/web';
+
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -7,32 +10,38 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="" />
-      <form>
-        <h1>Faça seu Logon</h1>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-        <Input name="nome" icon={FiUser} type="text" placeholder="Nome" />
-        <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="" />
+        <Form initialData={{ nome: 'Dddd' }} onSubmit={handleSubmit}>
+          <h1>Faça seu Logon</h1>
 
-        <Button>Cadastrar</Button>
-      </form>
+          <Input name="nome" icon={FiUser} type="text" placeholder="Nome" />
+          <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
 
-      <a href="/">
-        <FiChevronLeft />
-        Voltar para o Logon
-      </a>
-    </Content>
-  </Container>
-);
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="/">
+          <FiChevronLeft />
+          Voltar para o Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
